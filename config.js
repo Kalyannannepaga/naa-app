7c4cbb340d692a140959ef83e19e613939afd978
kalyan nannepaga <88704804+Kalyannannepaga@users.noreply.github.com>
2022-07-28 18:46:09 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+
+  const firebaseConfig = {
+    // apiKey: "AIzaSyC0OlXlzIbC-PVtVovGxJdRPHc7zXcLDRQ",
+    // authDomain: "plasma-27105.firebaseapp.com",
+    // databaseURL: "https://plasma-27105-default-rtdb.firebaseio.com",
+    // projectId: "plasma-27105",
+    // storageBucket: "plasma-27105.appspot.com",
+    // messagingSenderId: "222412919460",
+    // appId: "1:222412919460:web:547cfed4f4d78ed63f8c31"
+    apiKey: "AIzaSyDFzgfuOMxczEl-oXZZXLG5CUkKsP9HVSc",
+    authDomain: "get-the-details.firebaseapp.com",
+    databaseURL: "https://get-the-details-default-rtdb.firebaseio.com",
+    projectId: "get-the-details",
+    storageBucket: "get-the-details.appspot.com",
+    messagingSenderId: "926512138903",
+    appId: "1:926512138903:web:08061ca11af7977416b7d2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
